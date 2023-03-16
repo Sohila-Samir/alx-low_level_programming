f6067bc037c6f9708399a37e3b1d21fe50f7570f
Sohila-Samir <sohila.samir1911@gmail.com>
2023-03-16 22:58:17 +0200

    adding 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'programs store an array of charactres representing
+ * a string message to print later on using puts function'
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{
+	char msg[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(msg);
+
+	return (0);
+}
