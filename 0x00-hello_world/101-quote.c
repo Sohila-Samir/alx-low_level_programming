526c2b6e2c90c825aec918412956c2b09ca3e810
Sohila-Samir <sohila.samir1911@gmail.com>
2023-03-17 00:01:14 +0200

    adding 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: a C program that print out a message to the stderr
+ *
+ * Return: always 1 (faliure)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - %s, %s\n",
+	      "Dora Korpar", "2015-10-19");
+
+	return (1);
+}
