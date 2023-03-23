632e5bf5847f59a9f8185a6c0105a28ae04c5d47
Sohila-Samir <sohila.samir1911@gmail.com>
2023-03-23 17:49:55 +0200

    solving 0. Multiple mains task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests the correct output for the positive_or_negative()
+ * function with argument 0.
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(0);
+
+	return (0);
+}
