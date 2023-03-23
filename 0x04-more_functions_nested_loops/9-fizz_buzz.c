163a1230ead8625aa6347ab4a2c834a5fe501b66
Sohila-Samir <sohila.samir1911@gmail.com>
2023-03-23 14:16:37 +0200

    adding 9-fizz_buzz.c file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: fizzbuzz game to print numbers from 0 to
+ * 100.
+ *
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+	int i;
+	int max = 100;
+
+	for (i = 1; i <= max; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("%s", "Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s", "Buzz");
+		}
+		else if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i != max)
+		{
+			printf("%c", ' ');
+		}
+	}
+
+	printf("%c", '\n');
+
+	return (0);
+}
